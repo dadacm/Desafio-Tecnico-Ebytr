5fa31eeb0275e006f9175469722e814981f774f8
DADA <davimarque2000@gmail.com>
2021-11-04 17:58:05 -0300

    arquivos pr

--- desafio-tecnico/src/backend/index.js
+++ desafio-tecnico/src/backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const toDoRoute = require('./Routes/toDoListRoute');
+const cors  = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3001; 
 
