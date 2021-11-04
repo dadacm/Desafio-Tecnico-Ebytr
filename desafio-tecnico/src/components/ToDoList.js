62724b3246f7ce60af5df106c6fccaae5cd86bb6
DADA <davimarque2000@gmail.com>
2021-11-04 18:19:38 -0300

    organiza paginas front end

--- desafio-tecnico/src/components/ToDoList.js
+++ desafio-tecnico/src/components/ToDoList.js
@@ -1,17 +1,20 @@
+import React, { useEffect, useState } from "react";
+
 const ToDoList = () => {
-return(
-    <div>
-<header><h1>Minha Lista de Tarefas</h1></header>
-<p id = 'funcionamento'>Clique duas vezes em um item para marcá-lo como completo</p>
-<input type="text" id="texto-tarefa"/>
-<ol id="lista-tarefas">
-</ol>
-<button id = "criar-tarefa">criar tarefa</button>
-<button id = "apaga-tudo">apaga tudo</button>
-<button id = "remover-finalizados">remove finalizados</button>
-<button id = "salvar-tarefas">Salvar Lista</button>
-<button id = "mover-cima">mover para cima</button>
-<button id = "mover-baixo">mover para baixo</button>
-<button id = "remover-selecionado">remover selecionado</button>     </div>) };
+  const { list, setList } = useState([]);
+useEffect(() => {
+    fetch('http://localhost:3001/list')
+    .then((data) => data.json())
+    .then((task) => setList(task))
+    .then(console.log(list));
+
+})
+
+    return(
+        <ol>
+            <li>098</li>
+            {console.log(list)}
+        </ol>
+  ) };
 
 export default ToDoList;
--- desafio-tecnico/src/pages/Home.js
+++ desafio-tecnico/src/pages/Home.js
@@ -5,9 +5,21 @@ import  ToDoList  from "../components/ToDoList";
 const home  = () => {
   return (
     <BrowserRouter>
-      <h1>Ebytr To do list</h1>
-      <ToDoList/>
-    </BrowserRouter>
+      <div>
+       <h1>Ebytr To do list</h1>
+       <header><h1>Minha Lista de Tarefas</h1></header>
+       <p id = 'funcionamento'>Clique duas vezes em um item para marcá-lo como completo</p>
+       <input type="text" id="texto-tarefa"/>
+       <ol id="lista-tarefas"/>
+       <button id = "criar-tarefa">criar tarefa</button>
+       <button id = "apaga-tudo">apaga tudo</button>
+       <button id = "remover-finalizados">remove finalizados</button>
+       <button id = "salvar-tarefas">Salvar Lista</button>
+       <button id = "mover-cima">mover para cima</button>
+       <button id = "mover-baixo">mover para baixo</button>
+       <button id = "remover-selecionado">remover selecionado</button>
+    </div>
+  </BrowserRouter>
 );
 }
 
